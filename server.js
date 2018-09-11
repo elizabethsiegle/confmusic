fd9c6ae8aa0b20f188370f5206fc20a29edf2ae4
Jarod Reyes <jarodreyes@gmail.com>
2018-09-11 12:27:36 -0700

    Added soundparticipant (need to use a number Lizzie has)

--- server.js
+++ server.js
@@ -2,7 +2,6 @@
 //I think this works
 'use strict';
 const bodyParser = require('body-parser');
-const urlencoded = require('body-parser').urlencoded;
 const express = require('express');
 const http = require('http');
 const fs = require('fs')
@@ -19,9 +18,10 @@ let classic = 'https://demo.twilio.com/docs/classic.mp3'
 let okgodrum = 'audio/okgo-demo-drum.wav'
 let okgosound1 = 'audio/okgo-demo-sound1.wav'
 
+const client = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+
 // configuring middleware
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 app.use(bodyParser.raw({ type: 'audio/wav', limit: '50mb' }));
 app.use(express.static('audio/*')) //static 
 
@@ -31,9 +31,9 @@ app.use(express.static('audio/*')) //static
 app.post('/joinconference', (req, res) => {
   let twiml = new twilio.twiml.VoiceResponse(); 
   twiml.say('Welcome to the conference!');
-  const dial = twiml.dial();
+  let dial = twiml.dial();
   dial.conference('okgoconference1', {
-    startConferenceOnEnter: false,
+    startConferenceOnEnter: true,
   });
   //twiml.redirect('/soundparticipant') //don't stay in conference
   res.type('text/xml').send(twiml.toString());
@@ -44,21 +44,20 @@ app.post('/joinconference', (req, res) => {
 //curl or function to make music on command, do once
 //no one ever calls this number, request happens in background
 app.post('/soundparticipant', (req, res) => {
-  require('dotenv').load();
-  //create rest client
-  var client = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_TOKEN);
-  
-  // Generate a TwiML response
+
   //client starts call to okgoconference, play over the phone.
-  var call = client.calls
+  client.calls
   .create({
     //NOT SURE ABOUT THESE the url and to
-    url: '/rickroll', //point to twiml that rickrolls
-    to: 'okgoconference1', //num configured to /joinconference +17172971757
-    from: '+15612200834', //ghost number, 2nd num, configured to /soundparticipant
+    url: 'http://jreyes.ngrok.io/rickroll', //point to twiml that rickrolls
+    to: '+18654019875', //num configured to /joinconference +17172971757
+    from: '+12066505813', //ghost number, 2nd num, configured to /soundparticipant
   })
-  .then(call => console.log(call.sid));
-  res.type('text/xml').send("ok");
+  .then(call => {
+    console.log(call.sid);
+    res.type('text/xml').send(call.sid);
+  })
+  .catch(err => console.log(err))
 });
 
 app.post('/rickroll', (req, res) => {
@@ -85,13 +84,6 @@ app.post('/audio/okgo-demo-sound1.wav', (req, res) => {
 //effects, volume = connect inputs to right asset
 
 // start server
-http.createServer(function(request, response) {
-  console.log('listening on port 3000');
-  fs.readFile('pad.html', function(err, data) {
-    response.writeHead(200, {'Content-Type': 'text/html'});
-    response.write(data);
-    response.end();
-  });
-}).listen(3000);
+app.listen(3000, () => console.log('started server'));
 
   
