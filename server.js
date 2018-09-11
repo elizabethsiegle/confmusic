dcdf306071492f1546f9ff2135c2a622735d33d1
Lizzie Siegle <esiegle@brynmawr.edu>
2018-09-11 16:07:52 -0700

    commit to merge wah

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 //call +17172971757 configured with /joinconference , hear a welcome msg, redirect to /soundparticipant, get rickrolled
-//I think this works
+require('dotenv').load();
 'use strict';
 const bodyParser = require('body-parser');
-const urlencoded = require('body-parser').urlencoded;
 const express = require('express');
 const http = require('http');
 const fs = require('fs')
@@ -10,7 +9,6 @@ const path = require('path');
 var urllib = require('urllib');
 var url = require('url');
 const VoiceResponse = require('twilio').twiml.VoiceResponse
-const port = process.env.PORT || 3000;
 const app = express();
 const twilio = require('twilio');
 let twiml2 = new twilio.twiml.VoiceResponse();
@@ -19,55 +17,69 @@ let classic = 'https://demo.twilio.com/docs/classic.mp3'
 let okgodrum = 'audio/okgo-demo-drum.wav'
 let okgosound1 = 'audio/okgo-demo-sound1.wav'
 
+const client = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 // configuring middleware
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 app.use(bodyParser.raw({ type: 'audio/wav', limit: '50mb' }));
 app.use(express.static('audio/*')) //static 
+//jquery in node
+var jsdom = require('jsdom'),
+  { JSDOM } = jsdom,
+  jsdom_options = {
+  runScripts: "dangerously",
+  resources: "usable"
+};
+const { window } = new JSDOM('assets/pad.html');
+const $ = require('jquery')(window);
+var DOM = new JSDOM('assets/pad.html');
 
+app.post('/rickroll', (req, res) => {
+  //twiml play classic
+  let twiml2 = new twilio.twiml.VoiceResponse(); 
+  console.log("rickroll")
+  twiml2.play(classic); //classic url for now, need to work on okgo .wav files
+  res.type('text/xml').send(twiml2.toString());
+});
 
 // configure routes
 //right now: hear welcome message
 app.post('/joinconference', (req, res) => {
   let twiml = new twilio.twiml.VoiceResponse(); 
   twiml.say('Welcome to the conference!');
-  const dial = twiml.dial();
+  let dial = twiml.dial();
   dial.conference('okgoconference1', {
-    startConferenceOnEnter: false,
+    startConferenceOnEnter: true, //run once
   });
   //twiml.redirect('/soundparticipant') //don't stay in conference
   res.type('text/xml').send(twiml.toString());
 })
 
-//TODO: add if button clicked (in html file), later on: be listener, edit callsid
+//TODO: add if button clicked (in html file), be listener, edit callsid
 // RIGHT NOW: attendees call in to +17172971757, configured with /soundparticipant 
 //curl or function to make music on command, do once
 //no one ever calls this number, request happens in background
 app.post('/soundparticipant', (req, res) => {
-  require('dotenv').load();
-  //create rest client
-  var client = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_TOKEN);
-  
-  // Generate a TwiML response
-  //client starts call to okgoconference, play over the phone.
-  var call = client.calls
-  .create({
-    //NOT SURE ABOUT THESE the url and to
-    url: '/rickroll', //point to twiml that rickrolls
-    to: 'okgoconference1', //num configured to /joinconference +17172971757
-    from: '+15612200834', //ghost number, 2nd num, configured to /soundparticipant
-  })
-  .then(call => console.log(call.sid));
-  res.type('text/xml').send("ok");
+  var call = 'lol';
+   $(DOM.window.document).ready(function() {
+    $('#drum-pad span').click(function() {
+      var button = $(this).attr('data-key');
+      if(button == 'bass') {
+        //client starts call to okgoconference, play over the phone.
+        alert("bass clicked")
+        call = client.calls
+        .create({
+          url: 'https://lizzie.ngrok.io/rickroll', //point to twiml that rickrolls
+          to: '+17172971757', //num configured to /joinconference +17172971757
+          from: '+15612200834' //'+12066505813', //ghost number, 2nd num, configured to /soundparticipant
+        }) //create 
+        .then(call => {
+          console.log(call.sid);
+          res.type('text/xml').send(call.sid);
+        }).catch(err => console.log(err))
+      }
+    });
+  });
 });
-
-app.post('/rickroll', (req, res) => {
-  //twiml play classic
-  let twiml2 = new twilio.twiml.VoiceResponse(); 
-  console.log("hurr2")
-  twiml2.play(classic); //classic url for now, need to work on okgo .wav files
-  res.type('text/xml').send(twiml2.toString());
-})
 
 
 //make static wav file public to play
@@ -77,6 +89,11 @@ app.post('/audio/okgo-demo-sound1.wav', (req, res) => {
   res.type('audio/x-wav').send(twiml2.toString());
 });
 
+//serve up pad.html
+app.get('/', function(req, res){
+    res.sendFile('assets/pad.html', { root : __dirname});
+});
+app.use(express.static('assets')); //display background
 
 //latency of updating live-call?
 //unmuting ghost caller?
@@ -85,13 +102,6 @@ app.post('/audio/okgo-demo-sound1.wav', (req, res) => {
 //effects, volume = connect inputs to right asset
 
 // start server
-http.createServer(function(request, response) {
-  console.log('listening on port 3000');
-  fs.readFile('pad.html', function(err, data) {
-    response.writeHead(200, {'Content-Type': 'text/html'});
-    response.write(data);
-    response.end();
-  });
-}).listen(3000);
+app.listen(3000, () => console.log('started server'));
 
   
