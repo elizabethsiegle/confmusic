be2940e23cb3d35d048dbf5881504f7279174e46
Lizzie Siegle <esiegle@brynmawr.edu>
2018-09-12 22:19:57 -0700

    diff conf rooms temporarily, unable to customize welcome to conf room msg, wav urls not playing so need to click oh-yeah button

--- server.js
+++ server.js
@@ -13,23 +13,32 @@ const VoiceResponse = require('twilio').twiml.VoiceResponse
 const app = express();
 const twilio = require('twilio');
 let twiml2 = new twilio.twiml.VoiceResponse();
-let okgobass = 'audio/okgo-demo-bass.wav' 
+let okgobass = 'http://jardiohead.s3.amazonaws.com/okgo-demo-bass.wav' 
 let classic = 'https://demo.twilio.com/docs/classic.mp3'
 let cowbell = 'https://api.twilio.com/cowbell.mp3'
-let okgodrum = 'audio/okgo-demo-drum.wav'
-let okgosound1 = 'audio/okgo-demo-sound1.wav'
+let okgodrum = 'http://jardiohead.s3.amazonaws.com/okgo-demo-drum.wav'
+let okgosound1 = 'http://jardiohead.s3.amazonaws.com/okgo-demo-sound1.wav'
+let okgosound2 = 'http://jardiohead.s3.amazonaws.com/okgo-demo-sound2.wav'
 
 
 const client = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN)
 const fromNumber = process.env.TWILIO_NUMBER
 const toNumber = process.env.OKGO_CONF_NUMBER
 const callbackURL = process.env.CALLBACK_URL
+var crypto = require('crypto'),
+format = require('biguint-format');
 
+function randomC (qty) {
+  var x= crypto.randomBytes(qty);
+  return format(x, 'dec');
+}
+function random (low, high) {
+  return randomC(4)/Math.pow(2,4*8-1) * (high - low) + low;
+}
 // configuring middleware
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.raw({ type: 'audio/wav', limit: '50mb' }));
+app.use(bodyParser.raw({ type: 'audio/x-wav'})); //vnd.wave
 app.use(bodyParser.json());
-app.use(express.static('audio/*')) //static 
 
 // don't need jsdom because we can just make a request to our soundparticipant url from the client
 
@@ -48,15 +57,123 @@ app.post('/cowbell', (req, res) => {
   res.type('text/xml').send(twiml2.toString());
 });
 
+app.post('/bass', (req, res) => {
+  let twiml2 = new twilio.twiml.VoiceResponse(); 
+  console.log("bass")
+  twiml2.play(okgobass); //classic url for now, need to work on okgo .wav files
+  res.type('audio/x-wav').send(twiml2.toString()); //vnd.wave
+});
+
+app.post('/drum', (req, res) => {
+  let twiml2 = new twilio.twiml.VoiceResponse(); 
+  console.log("drum")
+  twiml2.play(okgodrum); //classic url for now, need to work on okgo .wav files
+  console.log(twiml2.toString());
+  //res.type('audio/wav').send(twiml2.toString());
+});
+
+app.post('/sound1', (req, res) => {
+  let twiml2 = new twilio.twiml.VoiceResponse(); 
+  console.log("sound1")
+  twiml2.play(okgosound1); //classic url for now, need to work on okgo .wav files
+  console.log(twiml2.toString());
+  //res.type('audio/wav').send(twiml2.toString());
+});
+app.post('/sound2', (req, res) => {
+  let twiml2 = new twilio.twiml.VoiceResponse(); 
+  console.log("sound2")
+  twiml2.play(okgosound2); //classic url for now, need to work on okgo .wav files
+  console.log(twiml2.toString());
+  //res.type('audio/wav').send(twiml2.toString());
+});
+//call initiated
+
 // configure routes
 //right now: hear welcome message
+var numCallers = [];
 app.post('/joinconference', (req, res) => {
-  let twiml = new twilio.twiml.VoiceResponse(); 
-  twiml.say('Welcome to the conference!');
+  var caller = req.body.From;
+  console.log("caller", caller)
+  if(caller != fromNumber) { //fromNumber is the ghost caller, tends to call a lot lmao
+    numCallers.push(caller);
+  }
+  console.log("numCallers arr ", numCallers); 
+  var rand = numCallers[Math.floor(Math.random() * numCallers.length)];
+  console.log("rand ", rand);
+  // var call1 = random(1, 3);
+  // var call2 = random(1, 3);
+  // var call3 = random(1, 3);
+  var twiml2ret = null;
+  let twiml = new twilio.twiml.VoiceResponse();
+  twiml.say('Get ready to be amazed by Okay Go!');
   let dial = twiml.dial();
-  dial.conference('okgoconference1', {
-    startConferenceOnEnter: true, //run once
-  });
+  if(rand == numCallers[0]) { //1
+    console.log("conf group 1")
+    twiml.say('Welcome to conference 1!'); //doesn't say 
+    dial.conference('okgoconference1', {
+      startConferenceOnEnter: true //run once
+    // muted: true //yolo
+    });
+    //twiml2ret = twiml;
+  }
+  else if (rand == numCallers[1]) {  //2
+    twiml.say('Welcome to conference 2!'); //doesn't say
+    console.log("conf 2");
+    dial.conference('okconference2', {
+      startConferenceOnEnter: true //run once
+      // muted: true //yolo
+    });
+    //twiml2ret = twiml2;
+  }
+  else if (rand == numCallers[2]) { //(numCallers.length == 3) {
+    console.log("conf 3");
+    twiml.say('Welcome to conference 3!');
+    dial.conference('okconference3', {
+      startConferenceOnEnter: true //run once
+      // muted: true //yolo
+    });
+    //twiml2ret = twiml3;
+  }
+  else if(numCallers.length == 4) {
+    twiml.say('Welcome to the conference 4!');
+    dial.conference('okconference4', {
+      startConferenceOnEnter: true //run once
+      // muted: true //yolo
+    });
+    // twiml2ret = twiml4;
+  }  
+  else if(numCallers.length == 5) {
+    twiml.say('Welcome to the conference 5!');
+    dial.conference('okconference5', {
+      startConferenceOnEnter: true //run once
+      // muted: true //yolo
+    });
+    // twiml2ret = twiml5;
+  }
+  else if(numCallers.length == 6) {
+    twiml.say('Welcome to the conference 6!');
+    dial.conference('okconference6', {
+      startConferenceOnEnter: true //run once
+      // muted: true //yolo
+    });
+    // twiml2ret = twiml6;
+  }
+  else if(numCallers.length == 7) {
+    twiml.say('Welcome to the conference 7!');
+    dial.conference('okconference7', {
+      startConferenceOnEnter: true //run once
+      // muted: true //yolo
+    });
+    // twiml2ret = twiml7;
+  }
+  else if(numCallers.length == 8) {
+    twiml.say('Welcome to the conference 8!');
+    dial.conference('okconference8', {
+      startConferenceOnEnter: true //run once
+      // muted: true //yolo
+    });
+    // twiml2ret = twiml8;
+  }
   res.type('text/xml').send(twiml.toString());
 })
 
@@ -67,7 +184,7 @@ app.post('/joinconference', (req, res) => {
 app.post('/soundparticipant', (req, res) => {
   console.log(req.body.button); //print in terminal, ngrok
   if(req.body.button == "oh-yeah") {
-    console.log("oh-yeah clicked, nodejs");
+    console.log("oh-yeah clicked, rickroll, nodejs");
     client.calls
     .create({
       url: callbackURL, //TODO currently rickrolls should instead point to listener for button click
@@ -80,10 +197,10 @@ app.post('/soundparticipant', (req, res) => {
     }).catch(err => console.log(err))
   } //if "oh-yeah"
   else if(req.body.button == "bass") {
-    console.log("oh-yeah clicked, nodejs");
+    console.log("bass clicked, nodejs");
     client.calls
     .create({
-      url: 'http://lizzie.ngrok.io/cowbell', //TODO currently rickrolls should instead point to listener for button click
+      url: 'http://lizzie.ngrok.io/bass', //TODO currently rickrolls should instead point to listener for button click
       to: toNumber, //num configured to /joinconference +17172971757
       from: fromNumber // any verified or twilio number
     })
@@ -92,16 +209,45 @@ app.post('/soundparticipant', (req, res) => {
         res.type('text/xml').send(call.sid);
     }).catch(err => console.log(err))
   }
-  
-  // create call from ghost participant
-  
-});
-
-//make static wav file public to play
-//DOESN'T WORK ATM
-app.post('/audio/okgo-demo-sound1.wav', (req, res) => {
-  twiml2.play(okgosound1);
-  res.type('audio/x-wav').send(twiml2.toString());
+  else if(req.body.button == "drum") {
+    console.log("drum clicked, nodejs");
+    client.calls
+    .create({
+      url: 'http://lizzie.ngrok.io/drum', //TODO currently rickrolls should instead point to listener for button click
+      to: toNumber, //num configured to /joinconference +17172971757
+      from: fromNumber // any verified or twilio number
+    })
+    .then(call => {
+      console.log(call.sid);
+        res.type('text/xml').send(call.sid);
+    }).catch(err => console.log(err))
+  }
+  else if(req.body.button == "sound-1") {
+    console.log("sound-1 clicked, nodejs");
+    client.calls
+    .create({
+      url: 'http://lizzie.ngrok.io/sound1', //TODO currently rickrolls should instead point to listener for button click
+      to: toNumber, //num configured to /joinconference +17172971757
+      from: fromNumber // any verified or twilio number
+    })
+    .then(call => {
+      console.log(call.sid);
+        res.type('text/xml').send(call.sid);
+    }).catch(err => console.log(err))
+  }
+  else if(req.body.button == "sound-2") {
+    console.log("sound-2 clicked, nodejs");
+    client.calls
+    .create({
+      url: 'http://lizzie.ngrok.io/sound-2', //TODO currently rickrolls should instead point to listener for button click
+      to: toNumber, //num configured to /joinconference +17172971757
+      from: fromNumber // any verified or twilio number
+    })
+    .then(call => {
+      console.log(call.sid);
+        res.type('text/xml').send(call.sid);
+    }).catch(err => console.log(err))
+  }
 });
 
 //serve up pad.html
